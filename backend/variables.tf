8c8f4c0e4434f19c8df969ca666acb6119170fee
ankurshashwat <1nkurshashwat@gmail.com>
2025-06-15 19:43:45 +0530

    Add S3 module for pipeline artifacts and Terraform state storage

---
+++ backend/variables.tf
@@ -0,0 +1,28 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "S3 bucket name for Terraform state"
+  type        = string
+  default     = "ivent-tf-state-bucket"
+}
+
+variable "table_name" {
+  description = "DynamoDB table name for state locking"
+  type        = string
+  default     = "ivent-tf-lock"
+}
+
+variable "project" {
+  description = "Project name for tagging"
+  type        = string
+  default     = "ivent"
+}
+
+variable "iam_user_arn" {
+  description = "IAM user ARN for bucket access"
+  type        = string
+}
