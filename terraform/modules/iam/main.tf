1a1b7b0d51e50de043212b8540306c70c5fde061
ankurshashwat <1nkurshashwat@gmail.com>
2025-07-07 18:53:51 +0530

    Add IAM module with roles and policies for Lambda, Github Actions including SNS subscribe permissions

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -180,7 +180,10 @@ resource "aws_iam_role_policy" "github_actions_policy" {
         Action = [
           "apigateway:*"
         ]
-        Resource = "arn:aws:apigateway:us-east-1::/restapis/egwv8azp80/*"
+        Resource = [
+          "arn:aws:apigateway:us-east-1::/restapis/egwv8azp80/*",
+          "arn:aws:apigateway:us-east-1::/restapis/3awqz21xg5/*"
+        ]
       },
       {
         Effect = "Allow"
