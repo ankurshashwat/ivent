e239f588edb7b7ba8faff577898919184aaa07a1
ankurshashwat <1nkurshashwat@gmail.com>
2025-07-18 22:12:20 +0530

    Add Cognito module for user authentication and API Gateway authorization

--- terraform/modules/cognito/main.tf
+++ terraform/modules/cognito/main.tf
@@ -14,3 +14,14 @@ resource "aws_cognito_user_pool_client" "client" {
   user_pool_id = aws_cognito_user_pool.user_pool.id
   explicit_auth_flows = ["ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_PASSWORD_AUTH"]
 }
+
+resource "aws_cognito_user" "test_user" {
+  user_pool_id = aws_cognito_user_pool.user_pool.id
+  username     = var.test_username
+  password     = var.test_password
+  
+  attributes = {
+    email          = "${var.test_username}@gmail.com"
+    email_verified = "true"
+  }
+}
--- terraform/modules/cognito/variables.tf
+++ terraform/modules/cognito/variables.tf
@@ -2,3 +2,14 @@ variable "user_pool_name" {
   description = "Name of the Cognito user pool"
   type        = string
 }
+
+variable "test_username" {
+  description = "Test user username for Cognito authentication"
+  type        = string
+}
+
+variable "test_password" {
+  description = "Test user password for Cognito authentication"
+  type        = string
+  sensitive   = true
+}
